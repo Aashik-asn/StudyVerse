b2083f783cf1e3b4454246797a31278e1623f008
Aashik asn <aashikrasheed786@gmail.com>
2025-09-16 20:33:01 +0530

    Addition of form

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
 from .forms import RoomForm
 # Create your views here.
@@ -15,10 +15,15 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context={'rooms':room}
+    context={'room':room}
     return render(request,'base/room.html',context)
 
 def createRoom(request):
     form = RoomForm()
+    if request.method=='POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
     context={'form':form}
     return render(request,'base/room_form.html',context)
