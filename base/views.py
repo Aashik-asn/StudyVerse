59cfac748b8fdfc941487b925ec6019b3d163dc5
Aashik asn <aashikrasheed786@gmail.com>
2025-09-16 22:08:24 +0530

    Include Nav and search

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from django.db.models import Q
 from .forms import RoomForm
+from .models import Room, Topic
 # Create your views here.
 # rooms=[
 #     {'id':1,'name':"Lets learn"},
@@ -9,8 +10,16 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms=Room.objects.all()
-    context={'rooms':rooms}
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+    rooms=Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        # | Q(host__username__icontains=q)
+        )
+    topics=Topic.objects.all()
+    room_count=rooms.count()
+    context={'rooms':rooms,'topics':topics,'room_count':room_count}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
