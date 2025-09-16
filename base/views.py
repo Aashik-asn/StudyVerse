f11b3b7de24f0200644df55bebf31814481de9e2
Aashik asn <aashikrasheed786@gmail.com>
2025-09-16 19:34:04 +0530

    First Upload

---
+++ base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Room
+from .forms import RoomForm
+# Create your views here.
+# rooms=[
+#     {'id':1,'name':"Lets learn"},
+#     {'id':2,'name':"Design"},
+#     {'id':3,'name':"Frontend dev"},
+# ]
+
+def home(request):
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room = Room.objects.get(id=pk)
+    context={'rooms':room}
+    return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
